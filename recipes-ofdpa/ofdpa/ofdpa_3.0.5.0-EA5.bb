bisdn/meta-ofdpa

commit 14a92c452a55f0311e352e3e97c569d1fe0d8689

ofdpa: allow setting l2/l3 memory allocation mode
Add a new command line switch to allow controlling the L2/L3 memory
allocation behaviour:

  -l, --l2l3mode=L2L3MODE    How to allocate memory to L2/L3 tables

L2L3MODE  = default
Valid modes are:
        default         use platform defaults
        balanced        allocate evenly to L2 and L3
        layer2          maximize L2 table size
        layer3          maximize L3 table size

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-ofdpa/ofdpa/ofdpa_3.0.5.0-EA5.bb
@@ -6,7 +6,7 @@ PV = "3.0.5.0-EA5+sdk-${SDK_VERSION}+gitAUTOINC+${@'${SRCREV_ofdpa}'[:10]}_${@'$
 
 PR = "r12"
 SDK_VERSION = "6.5.24"
-SRCREV_ofdpa = "1a21a1e29c5a3d21b2d26278b45c8c1b022cf014"
+SRCREV_ofdpa = "d3398e8dfa50948b9985fe289ba550baaf2732c6"
 SRCREV_sdk = "0b149ddfa3878e65eb217a11dddb999d3e205d03"
 
 DEPENDS = "python3 onl"

commit 87a264f9fc3e2e56c3367d224b4a38ab97cba158

ofdpa: mapping: fix L2/L3 memory alloction mode
Fixes: 14a92c452a55f0311e352e3e97c569d1fe0d8689

Signed-off-by: Roger Luethi <roger.luethi@bisdn.de>

recipes-ofdpa/ofdpa/ofdpa_3.0.5.0-EA5.bb
@@ -6,7 +6,7 @@ PV = "3.0.5.0-EA5+sdk-${SDK_VERSION}+gitAUTOINC+${@'${SRCREV_ofdpa}'[:10]}_${@'$
 
 PR = "r12"
 SDK_VERSION = "6.5.24"
-SRCREV_ofdpa = "d3398e8dfa50948b9985fe289ba550baaf2732c6"
+SRCREV_ofdpa = "257900c53522a220713e5fd2fe752f058ee8e769"
 SRCREV_sdk = "0b149ddfa3878e65eb217a11dddb999d3e205d03"
 
 DEPENDS = "python3 onl"