bisdn/meta-ofdpa

commit cef9203a5851f1b03cb7e9ef701d51f23029967a

ofdpa: package Trident 3 X7 firmware files
These are needed for BCM56870 based platforms like Accton AS7726-32X, so
package them up.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-ofdpa/ofdpa/ofdpa_3.0.5.0-EA5.bb
@@ -21,6 +21,7 @@ SRC_URI = " \
  http://repo.bisdn.de/nightly_builds/${MACHINE}/master/packages_latest-build/ipk/${MACHINE_ARCH}/ofagent_${@'${PV}'.replace('AUTOINC', '0')}-${PR}_${MACHINE_ARCH}.ipk;subdir=${P} \
  http://repo.bisdn.de/nightly_builds/${MACHINE}/master/packages_latest-build/ipk/${MACHINE_ARCH}/ofdpa_${@'${PV}'.replace('AUTOINC', '0')}-${PR}_${MACHINE_ARCH}.ipk;subdir=${P} \
  http://repo.bisdn.de/nightly_builds/${MACHINE}/master/packages_latest-build/ipk/${MACHINE_ARCH}/ofdpa-firmware-bcm56770_${@'${PV}'.replace('AUTOINC', '0')}-${PR}_${MACHINE_ARCH}.ipk;subdir=${P} \
+ http://repo.bisdn.de/nightly_builds/${MACHINE}/master/packages_latest-build/ipk/${MACHINE_ARCH}/ofdpa-firmware-bcm56870_${@'${PV}'.replace('AUTOINC', '0')}-${PR}_${MACHINE_ARCH}.ipk;subdir=${P} \
  http://repo.bisdn.de/nightly_builds/${MACHINE}/master/packages_latest-build/ipk/${MACHINE_ARCH}/python3-ofdpa_${@'${PV}'.replace('AUTOINC', '0')}-${PR}_${MACHINE_ARCH}.ipk;subdir=${P} \
 "
 
@@ -54,7 +55,7 @@ INHIBIT_PACKAGE_DEBUG_SPLIT  = "1"
 # this is machine specific
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 
-PACKAGES =+ "ofagent python3-${PN} ${PN}-firmware-bcm56770"
+PACKAGES =+ "ofagent python3-${PN} ${PN}-firmware-bcm56770 ${PN}-firmware-bcm56870"
 
 FILES_${PN} += "\
             ${sbindir}/ofdpa \
@@ -75,6 +76,10 @@ FILES_python3-${PN} = " \
                       "
 FILES_${PN}-firmware-bcm56770 = " \
            ${nonarch_base_libdir}/firmware/brcm/bcm56770*.pkg \
+           "
+
+FILES_${PN}-firmware-bcm56870 = " \
+           ${nonarch_base_libdir}/firmware/brcm/bcm56870*.pkg \
            "
 
 CONFFILES_${PN} = " \

commit c153a2e3873e5471d969790a612e048c2f79dab6

ofdpa: add missing RDEPENDS for Trident 3 X7 firmware
Add missing RDEPENDS for Trident 3 X7 files, which was accidentally
dropped when adding the required changes into the public repo.

Fixes: cef9203a5851 ("ofdpa: package Trident 3 X7 firmware files")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-ofdpa/ofdpa/ofdpa_3.0.5.0-EA5.bb
@@ -14,6 +14,7 @@ RDEPENDS_${PN} += "libgcc udev openbcm-gpl-modules"
 
 RDEPENDS_${PN} += " ${@bb.utils.contains('OFDPA_SWITCH_SUPPORT', 'BCM56370', '${PN}-firmware-bcm56370', '', d)}"
 RDEPENDS_${PN} += " ${@bb.utils.contains('OFDPA_SWITCH_SUPPORT', 'BCM56770', '${PN}-firmware-bcm56770', '', d)}"
+RDEPENDS_${PN} += " ${@bb.utils.contains('OFDPA_SWITCH_SUPPORT', 'BCM56870', '${PN}-firmware-bcm56870', '', d)}"
 
 # Nightly packages are regenerated regularily
 BB_STRICT_CHECKSUM = "0"