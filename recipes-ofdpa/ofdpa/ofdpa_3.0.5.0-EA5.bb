bisdn/meta-ofdpa

commit c109300737f96505c9650123adaa97be3dc72cfa

ofdpa: ship a ofdpa_log.cfg instead of creating it at start
To allow changing the log settings of the SDK persistently, ship a
ofdpa_log.cfg instead of forcing it to a fixed value on start in the
service file.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-ofdpa/ofdpa/ofdpa_3.0.5.0-EA5.bb
@@ -1,7 +1,7 @@
 DESCRIPTION = ""
 LICENSE = "CLOSED"
 
-PR = "r32"
+PR = "r33"
 SDK_VERSION = "6.5.24"
 SRCREV_ofdpa = "8d10ff02e0445267b458ea19191af5a5a63ce3d6"
 SRCREV_sdk = "0b149ddfa3878e65eb217a11dddb999d3e205d03"
@@ -38,6 +38,7 @@ PACKAGES =+ "\
 FILES:${PN} += "\
             ${sbindir}/ofdpa \
             ${sysconfdir}/default/ofdpa \
+            ${sysconfdir}/default/ofdpa_log.cfg \
             ${sysconfdir}/ofdpa \
             ${systemd_unitdir}/system/ofdpa.service \
             ${sbindir}/client* \
@@ -68,6 +69,7 @@ FILES:${PN}-firmware-bcm56870 = " \
 
 CONFFILES:${PN} = " \
   ${sysconfdir}/default/ofdpa \
+  ${sysconfdir}/default/ofdpa_log.cfg \
   ${sysconfdir}/ofdpa/rc.soc \
 "
 

commit 363b8590d4e3a155a74f152cfefdf2801fbeaf5b

ofdpa: actually include the log config in the package
Just adding the log config to the FILES isn't enough, we also need to
actually include it in the package.

Fixes: c109300737f9 ("ofdpa: ship a ofdpa_log.cfg instead of creating it at start")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-ofdpa/ofdpa/ofdpa_3.0.5.0-EA5.bb
@@ -1,7 +1,7 @@
 DESCRIPTION = ""
 LICENSE = "CLOSED"
 
-PR = "r34"
+PR = "r35"
 SDK_VERSION = "6.5.24"
 SRCREV_ofdpa = "8d10ff02e0445267b458ea19191af5a5a63ce3d6"
 SRCREV_sdk = "0b149ddfa3878e65eb217a11dddb999d3e205d03"