bisdn/meta-ofdpa

commit 2685834c920ae2f7ec08c7bbf19deffa1fe7200f

ofdpa: ofagent: fill manufacturer and hardware description from ONIE
Instead of hardcoding the manufacturer and hardware description in the
binary, let's read out the values from ONIE and fill the fields with
appropriate values at runtime.

Set the fallback values to "unknown", as we don't know on what hardware
we do run in this case.

Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-ofdpa/ofdpa/ofdpa_3.0.5.0-EA5.bb
@@ -6,7 +6,7 @@ PACKAGE_ARCH = "${MACHINE_ARCH}"
 
 PR = "r27"
 SDK_VERSION = "6.5.24"
-SRCREV_ofdpa = "7f83aa6ce52f1909cd0bc604e5218c3ce2c6597d"
+SRCREV_ofdpa = "c860f125285a7683fe97f52941911edd63ed7c98"
 SRCREV_sdk = "0b149ddfa3878e65eb217a11dddb999d3e205d03"
 
 inherit systemd python3-dir

commit 0486aca4ff37d3a792bd195bcbaa2a2e40f00d62

ofdpa: ofagent: fix copying strings from ONIE data
The fields from onie_info aren't fixed length strings, but rather
dynamically allocated ones. So we cannot just mempcy them, but need to
adhere to their actual length, else we will read over the buffers and
copy random data.

Additionally, the OpenFlow spec says that these fields should be
null-padded, so use strncpy, which will do that for us if the source
string length is less than the destination one.

While at it, make sure we do not try to read from NULL pointers.

Fixes leaking random (static) memory contents into the ofp_desc reply.

Fixes: 2685834c920a ("ofdpa: ofagent: fill manufacturer and hardware description from ONIE")
Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

recipes-ofdpa/ofdpa/ofdpa_3.0.5.0-EA5.bb
@@ -3,7 +3,7 @@ LICENSE = "CLOSED"
 
 PR = "r35"
 SDK_VERSION = "6.5.24"
-SRCREV_ofdpa = "8d10ff02e0445267b458ea19191af5a5a63ce3d6"
+SRCREV_ofdpa = "9655911ba4624eb67896c4c42c951ee07d633ecd"
 SRCREV_sdk = "0b149ddfa3878e65eb217a11dddb999d3e205d03"
 
 inherit systemd python3-dir